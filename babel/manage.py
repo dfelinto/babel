1649986e438e5235baf4f7b6bb6c176931da0112
Dalai Felinto <dfelinto@gmail.com>
2017-05-31 16:40:46 -0400

    Fix runserver entrypoint

--- babel/manage.py
+++ babel/manage.py
@@ -1,12 +1,9 @@
 import logging
-import requests
-from flask import current_app
-from flask_script import Manager
-from application.main import app
+from application.main import app as application
+
 
 log = logging.getLogger(__name__)
-manager = Manager(app)
 
 
-if __name__ == "__main__":
-    manager.run()
+if __name__ == '__main__':
+    application.run()
